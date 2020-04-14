42907fd60802fb2b6af4d72869c1e58ca17b5ea6
Peter Szalay <peterszalay@users.noreply.github.com>
2020-04-14 23:21:13 +0200

    type, interface basics
    Create type square, circle, attach method area() to each.
    
    Create a type shape interface as anything which has the area method.
    
    Create a func info which takes type shape and then prints the area.
    
    Create a value of type square and a value of type circle.
    
    Use func info to print the area of both.

---
+++ basics/exercise-01.go
@@ -0,0 +1,45 @@
+package main
+
+import (
+	"fmt"
+	"math"
+)
+
+//Create type square, circle, attach method area() to each.
+//Create a type shape interface as anything which has the area method.
+//Create a func info which takes type shape and then prints the area.
+//Create a value of type square and a value of type circle.
+//Use func info to print the area of both.
+
+type shape interface {
+	area() float64
+}
+
+type square struct {
+	side float64
+}
+
+type circle struct {
+	radius float64
+}
+
+func (c circle) area() float64 {
+	return math.Pi * c.radius * c.radius
+}
+
+func (s square) area() float64 {
+	return s.side * s.side
+}
+
+func info(s shape) {
+	fmt.Println(s.area())
+}
+
+func main(){
+	s := square{4.0}
+	c := circle{3.3}
+
+	info(s)
+	info(c)
+
+}
