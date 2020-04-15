6c145ae87f18e561fa8924d78ed561c373ce4604
Peter Szalay <peterszalay@users.noreply.github.com>
2020-04-15 23:05:50 +0200

    slice, range
    Initializing a slice using the composite literal

---
+++ basics/exercise-04.go
@@ -0,0 +1,23 @@
+package main
+
+import "fmt"
+
+// Initialize a SLICE of int using a composite literal;
+// print out the slice;
+// range over the slice printing out just the index;
+// range over the slice printing out both the index and the value
+
+func main() {
+	xs := []int{11, 22, 33, 44, 55}
+
+	fmt.Println(xs)
+	fmt.Println("Just the indexes: ")
+	for i := range xs {
+		fmt.Print(i,"\t")
+	}
+
+	fmt.Println("\n", "Indexes and values: ")
+	for i, v := range xs {
+		fmt.Println(i, " \t", v)
+	}
+}
